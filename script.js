da54a4b33a3c1e49ea9d24859ec7fdc32d86af2b
MaciejSzerwinski <101292211+MaciejSzerwinski@users.noreply.github.com>
2025-01-29 14:04:06 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function handleAnswer(answer) {
-    const response = document.getElementById('response');
-    if (answer === 'yes') {
-        response.textContent = 'Great! Keep coding!';
-    } else {
-        response.textContent = 'Oh no! Maybe give it another try!';
-    }
-}
